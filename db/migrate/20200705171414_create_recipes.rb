f42b4972c2608f40d0c3bc381b816efca5cb13b3
Pony <Pony1125@outlook.com>
2020-07-05 10:50:45 -0700

    Add recipe controller

--- db/migrate/20200705171414_create_recipes.rb
+++ db/migrate/20200705171414_create_recipes.rb
@@ -1,10 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.text :ingredients
-      t.text :instruction
-      t.string :image
+      t.string :name, null: false
+      t.text :ingredients, null: false
+      t.text :instruction, null: false
+      t.string :image, default: 'https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg'
 
       t.timestamps
     end
